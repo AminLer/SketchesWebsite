f6c3ee4500fe7c8c44f6cf5665e0305da6bd165c
AminLer <aminoumhamdi@gmail.com>
2016-11-08 19:37:06 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,15 +12,10 @@ function draw() {
   background(0);
   fill(255);
   
-  if(x < 400-x && y < 400-y){
+  if(x < 400 && x > 100){
     x+=speed;
-  }else if(y < 400-y && x == 400-x){
-    y+=speed;
-  }else if(x > 100-x && y == 400-y){
+  }else{
     x-=speed;
-  }else if(y > 100-y && x == 100){
-    y-=speed;
   }
-  
   rect(x, y, 50,50);
 }
